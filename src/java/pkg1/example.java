add0474a5c5850483ac552c308c15cec2a48c084
Tasnim <chokiwal@sheridancollege.ca>
2021-08-18 02:39:18 +0530

    that represents an image in my app.

---
+++ src/java/pkg1/example.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.pkg1;
+
+/**
+ *
+ * @author hp
+ */
+public class example {
+    public exampletring path){
+        this.path=path;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
